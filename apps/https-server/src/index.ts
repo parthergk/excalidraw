d63172f17e941ae9ed6893cce2df6afb107ed19c
parthergk <gauravkumar81464@gmail.com>
2025-03-19 22:36:06 +0530

    sign up

--- apps/https-server/src/index.ts
+++ apps/https-server/src/index.ts
@@ -3,24 +3,37 @@ import Jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
 import { prismaClient } from "@repo/db/client";
+import { UserSchema } from "@repo/common/types";
 
 const app = Express();
 
-app.post("/signup", (req: Request, res: Response) => {
-//   const { email, password } = req.body;
-  const newUser = prismaClient.user.create({
+app.post("/signup", async (req: Request, res: Response) => {
+  const parsedBody = UserSchema.safeParse(req.body);
+
+  if (!parsedBody.success) {
+    res.status(400).json({message: "invalid inputs"});
+    return;
+  }
+
+  await prismaClient.user.create({
     data: {
-        email: 'email',
-        password: 'password',
-        name: 'gk'
-    }
+      email: parsedBody.data.email,
+      password: parsedBody.data.password,
+      name: parsedBody.data.name,
+    },
   });
-  console.log("new user", newUser);
-  res.send("user created successfully");
+  res.status(200).json({message:"user created successfully"})
 });
 
 app.post("/signin", (req: Request, res: Response) => {
-  const { usrname, password } = req.body;
+  const parsedBody = UserSchema.safeParse(req.body);
+
+  if (!parsedBody.success) {
+    res.status(400).json({message: "invalid inputs"});
+    return;
+  }
+
+  
 
   const userId = 1;
   const token = Jwt.sign({ userId }, JWT_SECRET);
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,7 +1,11 @@
 import z from "zod";
 
 export const UserSchema = z.object({
-    username: z.string(),
+    email: z.string(),
     password: z.string().min(6),
     name: z.string()
+})
+
+export const RoomSchema = z.object({
+    room: z.string()
 })
