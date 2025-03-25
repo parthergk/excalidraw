14ff521dcd112e75af9693546b7d2dad1a219f67
parthergk <gauravkumar81464@gmail.com>
2025-03-25 09:47:50 +0530

    send message

--- apps/web/components/ChatClient.tsx
+++ apps/web/components/ChatClient.tsx
@@ -1,20 +1,57 @@
-"use client"
-import React, { useEffect } from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import { useSocket } from "../hooks/useSocket";
 
-const ChatClient = ({id ,messages}: {
-    id: string,
-    messages: {msg:string}[]
+const ChatClient = ({
+  id,
+  messages,
+}: {
+  id: string;
+  messages: { msg: string }[];
 }) => {
+  const [chats, setChats] = useState(messages);
+  const [currentChat, setCurrentChat] = useState("");
+  const { loading, socket } = useSocket();
+  useEffect(() => {
+    if (socket && !loading) {
+      socket.send(
+        JSON.stringify({
+          type: "join_room",
+          roomId: id,
+        })
+      );
 
-    useEffect(()=>{
-
-    },[id]);
+      socket.onmessage = (event)=>{
+        const parsedData = JSON.parse(event.data);
+        if (parsedData.type === "chat") {
+          setChats(c => [...c, {msg: parsedData.message}])
+        }
+      }
+    }
+  }, [socket, loading, id]);
 
   return (
-    <div>{
-        messages.length !== 0 ? messages.map((m)=><div>{m.msg}</div>) : "no message"
-    }</div>
-  )
-}
+    <div>
+      {chats.length !== 0
+        ? chats.map((m) => <div>{m.msg}</div>)
+        : "no message"}
+      <div>
+        <input
+          type="text"
+          value={currentChat}
+          onChange={(e) => setCurrentChat(e.target.value)}
+        />
+        <button onClick={()=>{
+          socket?.send(JSON.stringify({
+            type: "chat",
+            roomId: id,
+            messages: currentChat
+          }))
+          setCurrentChat("");
+        }}>send</button>
+      </div>
+    </div>
+  );
+};
 
-export default ChatClient
+export default ChatClient;
---
+++ apps/web/hooks/useSocket.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { WS_URL } from "../app/config";
+
+
+export function useSocket(){
+    const [loading, setLoading] = useState(true);
+    const [socket, setSocket] = useState<WebSocket>();
+
+    useEffect(()=>{
+        const ws = new WebSocket(`${WS_URL}?token=`);
+        ws.onopen = ()=>{
+            setLoading(false);
+            setSocket(ws);
+        }
+    },[])
+
+    return {
+        loading,
+        socket
+    }
+}
